c2d8ff27e7ac5fde6efa615b5c21b922ebb8c707
store13 <torefmly@gmail.com>
2020-04-28 15:58:32 -0400

    update HappyBirthday

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -1,7 +1,12 @@
 package day_1;
 
 public class HappyBirthday {
+    public static void main(String[] args) {
 
-    // Happy Birthday who were born today :)
-    System.out.println(" happy birthdays guys :)")
+
+        // Happy Birthday who were born today :)
+        System.out.println (" happy birthdays guys :)");
+    }
+
+
 }
